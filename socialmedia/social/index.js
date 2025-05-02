e5b028018a46d336c7f2354f274ccba4334c3381
divyanshi2611 <divyanshigupta2611@gmail.com>
2025-05-02 22:43:24 +0530

    Remove SnapChef and socialmedia from tracking

--- socialmedia/social/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SocialMediaApp from "./SocialMediaApp.js";
-
-// Create a root for rendering
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<SocialMediaApp />);
-
-SocialMediaApp();
